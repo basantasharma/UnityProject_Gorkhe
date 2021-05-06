1722e194cfcc201b85b02fa14aec1e5564609a49
basantasharma <gyawalibasanta0@gmail.com>
2021-05-06 15:05:21 +0545

    My initial Unity Gorkhe project Basic

--- Assets/Scripts/BulletFire.cs
+++ Assets/Scripts/BulletFire.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class BulletFire : MonoBehaviour
 {
+    public ParticleSystem MuzzleFlash;
+
+    //public float BulletFired = 0f;
     public float damage = 10f;
     public float range = 5f;
+
+    public float FireRate = 100f;
+    public float NextTimeToFire = 0f;
+    
 
     public Camera cam;
     // Start is called before the first frame update
@@ -17,25 +24,33 @@ public class BulletFire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetMouseButton(0) && Time.time >= NextTimeToFire && Cursor.lockState != CursorLockMode.None)
         {
+            NextTimeToFire = Time.time + (1f / FireRate) ;
             Shoot();
+            this.GetComponent<AudioSource>().Play();
+            //BulletFired += 1f;
+            //Debug.Log(BulletFired);
         }
         
     }
 
    void Shoot()
    {
+        MuzzleFlash.Play();
+       
         RaycastHit hit;
         if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
+            
+            //Debug.Log(hit.transform.name);
             Target target = hit.transform.GetComponent<Target>();
             if(target != null)
             {
                 target.TakeDamage(damage);
             }
         }
-
+       
+       
    }
 }
